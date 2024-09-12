408d19477bee1c303c967e69165faf60f204bb53
Dhamo1107 <dhamodharansathish4533@gmail.com>
2024-09-12 18:47:30 +0530

    Fixed failed test case

--- spec/auto_seeder_spec.rb
+++ spec/auto_seeder_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe AutoSeeder do
   it "has a version number" do
     expect(AutoSeeder::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
